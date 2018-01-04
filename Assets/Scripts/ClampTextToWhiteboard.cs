d8b588bbac8fe9754f0ad7d22470a95c671a3b7a
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-03 19:02:47 -0800

    initial commit

---
+++ Assets/Scripts/ClampTextToWhiteboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampTextToWhiteboard : MonoBehaviour {
+
+	public Text whiteboardText;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 textPos = Camera.main.WorldToScreenPoint(this.transform.position);
+		whiteboardText.transform.position = textPos;
+	}
+}
---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CountdownTimer : MonoBehaviour {
+	public Text text;
+	public float timeRemaining;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeRemaining > 0){
+			int minutes = (int) (timeRemaining / 60);
+			int seconds = (int) (timeRemaining % 60);
+			string secondsStr;
+			if (seconds < 10){
+				secondsStr = "0" + seconds.ToString();
+			} else {
+				secondsStr = seconds.ToString();
+			}
+			text.text = "Time Remaining: " + minutes.ToString() + ":" + secondsStr;
+			timeRemaining -= Time.deltaTime;
+		} else {
+			text.text = "Time Up";
+		}
+	}
+}
