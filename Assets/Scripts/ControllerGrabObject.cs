5e8da326cfdd2b90097e211ed54f53cfdb89165f
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-06 17:21:47 -0800

    created colliders on controllers and added script to allow player to grab rigidbody objects

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+    private SteamVR_TrackedObject trackedObj;
+
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // grab object if it collides with controller's collider
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // let the object go
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+
+    private void SetCollidingObject(Collider col)
+    {
+        // don't grab if you're already grabbing an object OR
+        // object doesn't have a RigidBody
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        } else {
+            collidingObject = col.gameObject;
+        }
+    }
+
+    // set object as candidate for holding
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // remove colliding object reference
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // FixedJoint is how to combine 2 RigidBodies so they move together
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+
+        if (GetComponent<FixedJoint>())
+        {
+            // remove FixedJoint and destroy the connection
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // So the object you're letting go of gets physics applied so it gets thrown "naturally" with an arc
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+
+        objectInHand = null;
+    }
+
+}
