9d1a442fa837045ce72d97fa5d89dcef0d087c7a
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-08 15:58:58 -0800

    add animation controller

--- Assets/Scripts/BotScripts/BotMovement.cs
+++ Assets/Scripts/BotScripts/BotMovement.cs
@@ -11,30 +11,42 @@ public class BotMovement : MonoBehaviour {
     NavMeshAgent nav;               // Reference to the nav mesh agent.
     float productivityScore;
 
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    public float amplitude;
+
+    private Vector3 tempPosition;
+
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        productivityScore = productivity.value;
         nav = GetComponent<NavMeshAgent>();
     }
     // Use this for initialization
     void Start () {
-		
-	}
+        tempPosition = transform.position;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (productivityScore < 25)
-        {
+        //if (productivity.value < 25)
+        //{
             // ... set the destination of the nav mesh agent to the player.
-            nav.SetDestination(player.position);
-        }
+            //nav.SetDestination(player.position);
+        //}
         // Otherwise...
-        else
-        {
+        //else
+        //{
             // ... disable the nav mesh agent.
-            nav.enabled = false;
-        }
+            //nav.enabled = false;
+        //}
+    }
+
+    void FixedUpdate()
+    {
+        tempPosition.x = transform.position.x + horizontalSpeed;
+        tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
     }
 }
