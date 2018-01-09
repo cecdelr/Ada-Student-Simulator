00f273405a0ca4f2527be7b7bb98b013fded6021
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-09 14:33:40 -0800

    refactored code and removed unused variables

--- Assets/Scripts/BotScripts/BotMovement.cs
+++ Assets/Scripts/BotScripts/BotMovement.cs
@@ -8,57 +8,42 @@ public class BotMovement : MonoBehaviour {
     public Slider productivity;
 
     Transform player;               // Reference to the player's position.
-    NavMeshAgent nav;               // Reference to the nav mesh agent.
 
     public float verticalSpeed;
     public float amplitude;
-	public float speed = 0.001f;
+	public float speed;
 
     private Vector3 tempPosition;
     private Vector3 originalPosition;
-
-    private float startTime;
-    private float journeyLength;
 
 
     void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        nav = GetComponent<NavMeshAgent>();
     }
     // Use this for initialization
     void Start () {
         tempPosition = transform.position;
         originalPosition = transform.position;
 
-        startTime = Time.time;
-        journeyLength = Vector3.Distance(originalPosition, player.position);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-		float distCovered = (Time.time - startTime) * speed;
-        float fracJourney = distCovered / journeyLength;
 
         if (productivity.value < 25 && player.position.x != transform.position.x && player.position.z != transform.position.z)
         {
             // go towards player
-            Debug.Log("gonna getcha");
-			transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * 1.0F);
-        
-        //if bot reaches the player, float up and down
+			transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * speed);
         } else if(productivity.value >= 25 && transform.position.x != originalPosition.x && transform.position.z != originalPosition.z)
         {
-            Debug.Log("byeeee");
-			transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * 1.0F);
-        } else
-        {
-        	Debug.Log("in position");
-        	tempPosition = transform.position;
-            tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
-            transform.position = tempPosition;
+        	// go to original position
+			transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * speed);
         }
+        //float up and down
+        tempPosition = transform.position;
+        tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+        transform.position = tempPosition;
     }
 }
