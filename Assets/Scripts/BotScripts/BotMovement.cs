c288ee566b7788c9293bb799ddbc36e104ee2645
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-08 21:03:43 -0800

    instructor bot now floats to top right of player but can't float back

--- Assets/Scripts/BotScripts/BotMovement.cs
+++ Assets/Scripts/BotScripts/BotMovement.cs
@@ -16,6 +16,10 @@ public class BotMovement : MonoBehaviour {
     public float amplitude;
 
     private Vector3 tempPosition;
+    private Vector3 originalPosition;
+
+    private float startTime;
+    private float journeyLength;
 
 
     void Awake()
@@ -26,27 +30,51 @@ public class BotMovement : MonoBehaviour {
     // Use this for initialization
     void Start () {
         tempPosition = transform.position;
+        originalPosition = transform.position;
+
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(originalPosition, player.position);
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         //if (productivity.value < 25)
         //{
-            // ... set the destination of the nav mesh agent to the player.
-            //nav.SetDestination(player.position);
+        // ... set the destination of the nav mesh agent to the player.
+        //nav.SetDestination(player.position);
         //}
         // Otherwise...
         //else
         //{
-            // ... disable the nav mesh agent.
-            //nav.enabled = false;
+        // ... disable the nav mesh agent.
+        //nav.enabled = false;
         //}
-    }
-
-    void FixedUpdate()
-    {
-        tempPosition.x = transform.position.x + horizontalSpeed;
-        tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
-        transform.position = tempPosition;
+        if (productivity.value < 25 && player.position.x != transform.position.x && player.position.z != transform.position.z)
+        {
+            // go towards player
+            Debug.Log("gonna getcha");
+            transform.position = Vector3.Lerp(originalPosition, player.position, Time.time - startTime);
+        
+        //if bot reaches the player, float up and down
+        } else if (productivity.value < 25 && player.position == transform.position)
+        {
+            Debug.Log("gotcha!");
+            tempPosition = transform.position;
+            tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+            transform.position = tempPosition;
+        } else if(productivity.value >= 25 && transform.position != originalPosition)
+        {
+            Debug.Log("byeeee");
+            transform.position = Vector3.Lerp(player.position, originalPosition, Time.time - startTime);
+            // go back to original position
+            //tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+            //transform.position = tempPosition;
+        } else
+        {
+            tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
+            transform.position = tempPosition;
+        }
     }
 }
