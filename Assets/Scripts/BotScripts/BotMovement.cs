2acc805f33761706e1e328888b1aa9376232a39a
Isaac Del Rosario <cecdelr@gmail.com>
2018-01-09 14:17:12 -0800

    instructor bot can move back and forth according to productivity score

--- Assets/Scripts/BotScripts/BotMovement.cs
+++ Assets/Scripts/BotScripts/BotMovement.cs
@@ -9,11 +9,10 @@ public class BotMovement : MonoBehaviour {
 
     Transform player;               // Reference to the player's position.
     NavMeshAgent nav;               // Reference to the nav mesh agent.
-    float productivityScore;
 
-    public float horizontalSpeed;
     public float verticalSpeed;
     public float amplitude;
+	public float speed = 0.001f;
 
     private Vector3 tempPosition;
     private Vector3 originalPosition;
@@ -40,39 +39,24 @@ public class BotMovement : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        //if (productivity.value < 25)
-        //{
-        // ... set the destination of the nav mesh agent to the player.
-        //nav.SetDestination(player.position);
-        //}
-        // Otherwise...
-        //else
-        //{
-        // ... disable the nav mesh agent.
-        //nav.enabled = false;
-        //}
+		float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+
         if (productivity.value < 25 && player.position.x != transform.position.x && player.position.z != transform.position.z)
         {
             // go towards player
             Debug.Log("gonna getcha");
-            transform.position = Vector3.Lerp(originalPosition, player.position, Time.time - startTime);
+			transform.position = Vector3.Lerp(transform.position, player.position, Time.deltaTime * 1.0F);
         
         //if bot reaches the player, float up and down
-        } else if (productivity.value < 25 && player.position == transform.position)
-        {
-            Debug.Log("gotcha!");
-            tempPosition = transform.position;
-            tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
-            transform.position = tempPosition;
-        } else if(productivity.value >= 25 && transform.position != originalPosition)
+        } else if(productivity.value >= 25 && transform.position.x != originalPosition.x && transform.position.z != originalPosition.z)
         {
             Debug.Log("byeeee");
-            transform.position = Vector3.Lerp(player.position, originalPosition, Time.time - startTime);
-            // go back to original position
-            //tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
-            //transform.position = tempPosition;
+			transform.position = Vector3.Lerp(transform.position, originalPosition, Time.deltaTime * 1.0F);
         } else
         {
+        	Debug.Log("in position");
+        	tempPosition = transform.position;
             tempPosition.y = transform.position.y + Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude;
             transform.position = tempPosition;
         }
